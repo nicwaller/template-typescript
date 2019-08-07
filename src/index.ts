592e93848a26cbc166979aea9edaf808cd415b9b
Nic Waller <code@nicwaller.com>
2019-08-07 09:36:10 -0700

    Bring in Lo-Dash
    Because I really crave a proper iterator package, and apparently Lo-Dash is the cool and modern replacement for Underscore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import pino from 'pino';
+import * as _ from 'lodash';
 const logger = pino({
     name: 'app-name',
     level: 'debug'
 });
-const helloWorld: string = 'Hello World';
-logger.info(helloWorld);
+const helloWorld = 'What a wonderful world.';
+for (let part of _.chunk(helloWorld.split(' '), 2)) {
+    logger.info(part.join(' '));
+}
